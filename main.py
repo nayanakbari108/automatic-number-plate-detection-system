9af3af2a2d32c32b3236d6ca1827732595ffbc0b
nayanakbari108 <142861899+nayanakbari108@users.noreply.github.com>
2023-10-09 22:23:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+frameWidth = 700    #Frame Width
+franeHeight = 500   # Frame Height
+
+plateCascade = cv2.CascadeClassifier("C:\\Users\\91998\\Desktop\\projects python\\ANP Dectection\\haarcascade_russian_plate_number.xml")
+minArea = 500
+
+cap =cv2.VideoCapture(0)
+cap.set(3,frameWidth)
+cap.set(4,franeHeight)
+cap.set(10,150)
+count = 0
+
+while True:
+    success , img  = cap.read()
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    numberPlates = plateCascade .detectMultiScale(imgGray, 1.1, 4)
+
+    for (x, y, w, h) in numberPlates:
+        area = w*h
+        if area > minArea:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.putText(img,"Number Plate",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+            imgRoi = img[y:y+h,x:x+w]
+            cv2.imshow("ROI",imgRoi)
+    cv2.imshow("Result",img)
+    if cv2.waitKey(1) & 0xFF ==ord('s'):
+        
+        cv2.imwrite("C:\\Users\\91998\\Desktop\\projects python\\ANP Dectection\\Images\\image"+str(count)+".jpg",imgRoi)
+        cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
+        cv2.putText(img,"Scan Saved",(15,265),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),2)
+        cv2.imshow("Result",img)
+        cv2.waitKey(500)
+        count+=1
+       
+          
+        
